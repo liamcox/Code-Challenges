9f7c679e3cb497ba71d78b8c57d1725d66103a16
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2021-02-09 14:00:22 -0600

    did some code challenges

---
+++ index.js
@@ -0,0 +1,15 @@
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return True if yes, False otherwise :)
+
+function hero(bullets, dragons) {
+    return bullets >= dragons * 2;
+}
+
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+// Simples!
+function fixTheMeerkat(arr) {
+    return arr.reverse();
+}

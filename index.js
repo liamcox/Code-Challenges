fbb57ecdda184ed1fe3c7969aa579f6a6eb4253c
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2021-02-10 11:31:47 -0600

    challenges

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ function hero(bullets, dragons) {
 function fixTheMeerkat(arr) {
     return arr.reverse();
 }
+
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+function bonusTime(salary, bonus) {
+    // your code here
+    if (bonus === true) {
+        let money = salary * 10;
+        return `£${money.toString()}`;
+    } else {
+        return `£${salary}`;
+    }
+}
+
+// Your task is to find the first element of an array that is not consecutive.
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+// If the whole array is consecutive then return null2.
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1];
+        }
+        return "All numbers are in order!"
+    }
+}
